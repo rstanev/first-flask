453c7c8d0480fd0b5019958372420f452072c7ee
Roger Stanev <rstanev@gmail.com>
2016-08-27 12:54:22 -0700

    First flask-demo in GitHub

---
+++ app.py
@@ -0,0 +1,16 @@
+# Testing GitHub - rstanev@gmail.com
+
+from flask import Flask, render_template, request, redirect
+
+app = Flask(__name__)
+
+@app.route('/')
+def main():
+  return redirect('/index')
+
+@app.route('/index')
+def index():
+  return render_template('index.html')
+
+if __name__ == '__main__':
+  app.run(port=33507)
